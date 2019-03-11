7cfdad764a24d8390b19724abd6c1634f53c1b8c
David Hernandez <davidhernandezruiz@gmail.com>
2019-03-11 11:50:33 +0100

    Extracts the configuration form from the main app file to a new component.

--- app/App.js
+++ app/App.js
@@ -1,30 +1,9 @@
 import React from 'react';
-import { AsyncStorage, StyleSheet, Text, View } from 'react-native';
-import { Input, Button, Header } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { View } from 'react-native';
+import { Header } from 'react-native-elements';
+import ConfigForm from './ConfigForm.js'
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {phone:''}
-  }
-
-  componentDidMount() {
-    this._retrievePhone()
-  }
-
-  async _retrievePhone() {
-    const phone = await AsyncStorage.getItem('phone')
-    if (null !== phone) {
-      this.setState({phone: phone})
-    }
-  }
-
-  setPhone = (value) => {
-    AsyncStorage.setItem('phone', value);
-    this.setState({phone: value});
-  }
-
   render() {
     return (
       <View>
@@ -32,35 +11,8 @@ export default class App extends React.Component {
           centerComponent={{ text: 'Drop Alert', style: { color: '#fff', fontSize: 24 } }}
         />
 
-        <Input
-          value={this.state.phone}
-          onChangeText = {this.setPhone}
-          placeholder='Teléfono'
-          keyboardType='numeric'
-          errorMessage='Introduce un teléfono correcto'
-          errorStyle={{ color: 'red' }}
-          leftIcon={
-            <Icon
-              name='mobile-phone'
-              size={24}
-              color='black'
-            />
-          }
-        />
-
-        <Button onPress={this._savePhone}
-          title="Guardar"
-        />
+        <ConfigForm></ConfigForm>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/ConfigForm.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { AsyncStorage, Text, View } from 'react-native';
+import { Input, Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default class ConfigForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {phone:'', error: true, isSaved: false}
+  }
+
+  componentDidMount() {
+    this._retrievePhone()
+  }
+
+  async _retrievePhone() {
+    const phone = await AsyncStorage.getItem('phone')
+    if (null !== phone) {
+      this.setState({phone: phone, error: false, isSaved: true})
+    }
+  }
+
+  setPhone = (phone) => {
+    this.setState({phone: phone})
+    this.validatePhone(phone)
+  }
+
+  savePhone = () => {
+    const phone = this.state.phone
+
+    this.validatePhone(phone)
+
+    if (this.isValidPhone(phone)) {
+      AsyncStorage.setItem('phone', phone)
+      this.setState({isSaved: true})
+    }
+  }
+
+  isValidPhone(phone) {
+    return (!isNaN(phone) && isFinite(phone) && 9 == phone.length)
+  }
+
+  validatePhone(phone) {
+    this.setState({error: !this.isValidPhone(phone)})
+  }
+
+  renderSuccessMessage() {
+    if (this.state.isSaved) {
+      return (<Text>Teléfono guardado correctamente</Text>)
+    }
+    else {
+      return (<Text/>)
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Input
+          label='Teléfono de contacto en caso de emergencia'
+          value={this.state.phone}
+          onChangeText = {this.setPhone}
+          placeholder='Teléfono'
+          keyboardType='numeric'
+          errorMessage={ this.state.error ? 'Introduce un teléfono correcto' : '' }
+          errorStyle={{ color: 'red' }}
+          leftIcon={
+            <Icon
+              name='mobile-phone'
+              size={24}
+              color='black'
+            />
+          }
+        />
+
+        { this.renderSuccessMessage() }
+
+        <Button onPress={this.savePhone}
+          title="Guardar"
+        />
+      </View>
+    );
+  }
+}
