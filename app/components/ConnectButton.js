1b90cb49752fa1dbef5e8f821c75c65f8cc2ed89
Hynnot <hynnot@gmail.com>
2019-03-25 14:07:59 +0100

    First steps to connect devices via bluetooth

--- app/components/ConnectButton.js
+++ app/components/ConnectButton.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 import { Button } from 'react-native-elements'
-import { BleManager } from 'react-native-ble-plx'
+import { BleManager, LogLevel } from 'react-native-ble-plx'
 
 export default class ConfigForm extends React.Component {
   constructor(props) {
     super(props)
     this.bleManager = new BleManager()
+    this.bleManager.setLogLevel(LogLevel.Verbose)
     this.state = { bluetoothState: 'PoweredOff', errors: [] }
   }
 
@@ -18,16 +19,30 @@ export default class ConfigForm extends React.Component {
 
     checkConnection = () => {
         this.bleManager.startDeviceScan(null, null, (error, device) => {
-            console.log(error)
-            console.log(device)
+            console.log(device.name)
+	    console.log(device.id)
+
             if (error) {
                 // Handle error (scanning will be stopped automatically)
+		console.log(error)
                 this.bleManager.stopDeviceScan()
                 return
             }
 
             // Check if it is a device you are looking for based on advertisement data
             // or other criteria.
+	    if (device.isConnected()) {
+ 	        console.log(device.name)
+	        console.log(device.id)
+
+		console.log('Dispositivo conectado!')
+	    } else {
+	    	console.log('Dispositivo no conectado')
+		console.log(device.name)
+		console.log(device.id)
+
+	    }
+
 
             if (device.name === 'OurDevice') {
                 if (device.isConnected()) {
